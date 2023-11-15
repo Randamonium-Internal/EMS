576f743f7eed990bebc28839398dd47c454ac308
FlopsiBunny <admin@randamonium.co.nz>
2023-11-15 17:17:12 +1300

    Created Node Model

--- node/admin.py
+++ node/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Node
 
-# Register your models here.
+@admin.register(Node)
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_master', 'ip_address', 'last_check_in')
+    list_filter = ('is_master',)
+    search_fields = ('name', 'ip_address')
--- node/models.py
+++ node/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Node(models.Model):
+    name = models.CharField(max_length=100)
+    is_master = models.BooleanField(default=False)
+    ip_address = models.GenericIPAddressField()
+    last_check_in = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.is_master:
+            # Ensure there's only one master node
+            if Node.objects.filter(is_master=True).exclude(pk=self.pk).exists():
+                raise ValidationError("There can only be one master node.")
+        super(Node, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} ({'Master' if self.is_master else 'Slave'})"
